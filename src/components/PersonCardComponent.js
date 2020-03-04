14a17843252aa4bb94ca7abf6c4b476d0c1ecca8
Sasha <58642542+alro-way@users.noreply.github.com>
2020-03-03 16:42:37 -0800

    Putting it Together - is added

--- src/components/PersonCardComponent.js
+++ src/components/PersonCardComponent.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 
 
+
 class PersonCardComponent extends Component{
     constructor(props) {
         super(props);
-        }
-        render() {
-            const {firstname, lastname, age, haircolor} = this.props;
-            
-            return (
-                <div>
-                    <h2>{firstname} {lastname} </h2>
-                    <p>{age}</p>
-                    <p>{haircolor}</p>
-                </div>
-            );
+        this.state = {
+            clickage: props.age
         }
     }
+    handleClick = () => {
+        this.setState({
+            clickage: this.state.clickage + 1
+        })
+    }
+
+    render() {
+        const {firstname, lastname, haircolor} = this.props;
+        
+        return (
+            <div>
+                <h2>{firstname} {lastname} </h2>
+                <p>{this.state.clickage}</p>
+                <p>{haircolor}</p>
+                <button onClick={this.handleClick}>Birthday button for {firstname} {lastname}</button>
+            </div>
+        );
+    }
+}
+
+    
 export default PersonCardComponent;
 
