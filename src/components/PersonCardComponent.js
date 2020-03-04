82d99a6dc7fa8e9af37a376604dbbf0fc3b08245
Sasha <58642542+alro-way@users.noreply.github.com>
2020-03-04 12:02:39 -0800

    Big Inversion assignment is added

--- src/components/PersonCardComponent.js
+++ src/components/PersonCardComponent.js
@@ -1,34 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
 
+// As a class component:
 
+// class PersonCardComponent extends React.Component{
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             clickage: props.age
+//         }
+//     }
+//     handleClick = () => {
+//         this.setState({
+//             clickage: this.state.clickage + 1
+//         })
+//     }
 
-class PersonCardComponent extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            clickage: props.age
-        }
-    }
-    handleClick = () => {
-        this.setState({
-            clickage: this.state.clickage + 1
-        })
-    }
-
-    render() {
-        const {firstname, lastname, haircolor} = this.props;
+//     render() {
+//         const {firstname, lastname, haircolor} = this.props;
         
-        return (
-            <div>
-                <h2>{firstname} {lastname} </h2>
-                <p>{this.state.clickage}</p>
-                <p>{haircolor}</p>
-                <button onClick={this.handleClick}>Birthday button for {firstname} {lastname}</button>
-            </div>
-        );
-    }
-}
+//         return (
+//             <div>
+//                 <h2>{firstname} {lastname} </h2>
+//                 <p>{this.state.clickage}</p>
+//                 <p>{haircolor}</p>
+//                 <button onClick={this.handleClick}>Birthday button for {firstname} {lastname}</button>
+//             </div>
+//         );
+//     }
+// }
 
     
+// export default PersonCardComponent;
+
+//As a functional component:
+
+const PersonCardComponent = props => {
+        return (
+            <div>
+                <h2>{props.firstname} {props.lastname} </h2>
+                <p>Age: {props.age}</p>
+                <p>Hair: {props.haircolor}</p>
+            </div>
+        );
+
+}
 export default PersonCardComponent;
 
